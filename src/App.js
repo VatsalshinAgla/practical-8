1b18854b6015382228d984b5a5672042ff2d4ce7
VatsalshinAgla <vatsalshingala99@gmail.com>
2022-04-19 14:44:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { Routes, Route,Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import Home from "./components/Home";
+import SignUp from "./components/SignUp";
+import "./App.css";
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
+
+function App() {
+  const { isAuthenticated } = useSelector((state) => state);
+  return (
+    <div className="app">
+      <Routes>
+        <Route
+          path="/signup"
+          element={
+            <PublicRoute isAuthenticated={isAuthenticated} component={SignUp} />
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated} component={Home} />
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <Navigate to={'/home'} />
+          }
+        />
+      </Routes>
+    
+    </div>
+  );
+}
+
+export default App;
